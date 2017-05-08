38c29e7bde789115d53ec3dc35bee170e3bd61a5
msjo91 <msjo91@gmail.com>
2017-05-08 16:10:18 +0900

    Form basic login_facebook structure

--- django_app/member/urls.py
+++ django_app/member/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'member'
 urlpatterns = [
     url(r'^login/$', views.login, name='login'),
+    url(r'^login/facebook/$', views.login_facebook, name='login_facebook')
 ]
--- django_app/member/views.py
+++ django_app/member/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from config import settings
+
 
 def login(request):
-    context = {}
+    facebook_app_id = settings.config['facebook']['app_id']
+    context = {
+        'facebook_app_id': facebook_app_id,
+    }
     return render(request, 'member/login.html', context)
+
+
+def login_facebook(request):
+    print(request.GET)
