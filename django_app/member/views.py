3866a2dccd13521d691462cbf38a37ba8e348c05
msjo91 <msjo91@gmail.com>
2017-05-08 17:02:00 +0900

    Upper case several variables for emphasis

--- django_app/member/views.py
+++ django_app/member/views.py
@@ -3,22 +3,22 @@ from django.shortcuts import render
 
 from config import settings
 
-app_id = settings.config['facebook']['app_id']
+APP_ID = settings.config['facebook']['app_id']
 
 
 def login(request):
     context = {
-        'app_id': app_id,
+        'app_id': APP_ID,
     }
     return render(request, 'member/login.html', context)
 
 
 def login_facebook(request):
-    redirect_uri = 'http://localhost:8080/member/login/facebook/'
-    secret_code = settings.config['facebook']['secret_code']
-    app_access_token = '{app_id}|{secret_code}'.format(
-        app_id=app_id,
-        secret_code=secret_code
+    REDIRECT_URI = 'http://localhost:8080/member/login/facebook/'
+    SECRET_CODE = settings.config['facebook']['secret_code']
+    APP_ACCESS_TOKEN = '{app_id}|{secret_code}'.format(
+        app_id=APP_ID,
+        secret_code=SECRET_CODE
     )
 
     # Redirect Facebook loign from login()
@@ -26,26 +26,26 @@ def login_facebook(request):
     # Return to login_facebook() using 'redirect_uri'
     # Then...
     if request.GET.get('code'):
-        code = request.GET.get('code')
+        CODE = request.GET.get('code')
 
         # Request access_token using received 'code'
         url_request_access_token = 'https://graph.facebook.com/v2.9/oauth/access_token'
         params = {
-            'client_id': app_id,
-            'redirect_uri': redirect_uri,
-            'client_secret': secret_code,
-            'code': code,
+            'client_id': APP_ID,
+            'redirect_uri': REDIRECT_URI,
+            'client_secret': SECRET_CODE,
+            'code': CODE,
         }
         r = requests.get(url_request_access_token, params=params)
         dict_access_token = r.json()
-        user_access_token = dict_access_token['access_token']
+        USER_ACCESS_TOKEN = dict_access_token['access_token']
 
         # Authenticate token using user_access_token and app_access_token
         url_debug_token = 'https://graph.facebook.com/debug_token'
         params = {
-            'input_token': user_access_token,
-            'access_token': app_access_token
+            'input_token': USER_ACCESS_TOKEN,
+            'access_token': APP_ACCESS_TOKEN
         }
         r = requests.get(url_debug_token, params=params)
         dict_debug_token = r.jsoin()
-        user_id = dict_debug_token['data']['user_id']
+        USER_ID = dict_debug_token['data']['user_id']
