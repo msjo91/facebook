3a8bd3145df57498115e2e5a1aedde2e32eb6895
msjo91 <msjo91@gmail.com>
2017-05-10 16:47:16 +0900

    Create url for logout

--- django_app/member/urls.py
+++ django_app/member/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'member'
 urlpatterns = [
     url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
     url(r'^login/facebook/$', views.login_facebook, name='login_facebook')
 ]
