0473e638b2020eeb0b6f1d444349b9f9e69beca2
msjo91 <msjo91@gmail.com>
2017-05-11 17:00:43 +0900

    Remove unncessary imports

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -1,6 +1,5 @@
-from django.contrib.auth.models import AbstractUser, UserManager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils.crypto import get_random_string
 
 
 # class MyUserManager(UserManager):
